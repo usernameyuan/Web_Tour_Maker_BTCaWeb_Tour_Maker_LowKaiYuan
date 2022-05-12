2b9cfbde4bb697f44dd4d4a380f291a86f498c10
usernameyuan <lowkaivuan@gmail.com>
2022-05-12 19:37:50 +0800

    WebTourMaker Assingment
    Assignment for Webtourmaker BukitTimah Campus

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-c-j-koh-law-library",
+      "name": "C J Koh Law Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.1596687810966717,
+        "pitch": 0.14554831885080333,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5308457247987661,
+          "pitch": 0.6660774290122458,
+          "rotation": 3.141592653589793,
+          "target": "1-stone-plate-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.23814506056765872,
+          "pitch": 0.6232235632146335,
+          "title": "Click to proceed&nbsp;",
+          "text": "Stone Plate&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-stone-plate-of-nus-law",
+      "name": "Stone Plate of NUS Law",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.057805030206646,
+        "pitch": 0.029350495695384282,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3893752063184355,
+          "pitch": 0.43620191046361434,
+          "rotation": 4.71238898038469,
+          "target": "2-the-summit-canteen"
+        },
+        {
+          "yaw": -2.6291615389901573,
+          "pitch": 0.9229543010404839,
+          "rotation": 3.141592653589793,
+          "target": "0-c-j-koh-law-library"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5402816455738124,
+          "pitch": 0.3440960991759354,
+          "title": "Click to proceed&nbsp;",
+          "text": "Summit Canteen"
+        },
+        {
+          "yaw": -2.522392527832679,
+          "pitch": 0.7424921441195149,
+          "title": "Click to proceed&nbsp;",
+          "text": "CJ Koh Library"
+        }
+      ]
+    },
+    {
+      "id": "2-the-summit-canteen",
+      "name": "The Summit Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.8357657858503309,
+        "pitch": 0.17970104037957668,
+        "fov": 1.2723536005922274
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5570887660687145,
+          "pitch": 0.47259252972409627,
+          "rotation": 0,
+          "target": "1-stone-plate-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3908216321074676,
+          "pitch": 0.5458138674931732,
+          "title": "Click to proceed&nbsp;",
+          "text": "Stone Plate&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
